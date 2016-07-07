512044afcf1753e962d7ce66eeaf2f3035a3f224
JMSolo <jmsolodesigns@gmail.com>
2016-07-07 07:26:57 -0500

    update the diskspace plugin for upcoming 2.4

--- diskspace/action.php
+++ diskspace/action.php
@@ -1,12 +1,24 @@
 <?php
 
-//--------------------------------------------------------------------------------
-// [Quick Box - action.php modified for quota systems use]
-//
-// GitHub:   https://github.com/JMSDOnline/quick-box
-// Author:   Jason Matthews
-// URL:      https://jmsolodesigns.com/code-projects/quick-box/seedbox-installer
-//--------------------------------------------------------------------------------
+#################################################################################
+##  [Quick Box - action.php modified for quota systems use]
+#################################################################################
+# GITHUB REPOS
+# GitHub _ packages  :   https://github.com/QuickBox/quickbox_rutorrent-plugins
+# LOCAL REPOS
+# Local _ packages   :   ~/QuickBox/rtplugins
+# Author             :   QuickBox.IO
+# URL                :   https://plaza.quickbox.io
+#
+# QuickBox Copyright (C) 2016 QuickBox.io
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+#
+#################################################################################
 
   require_once( '../../php/util.php' );
   if (isset($quotaUser) && file_exists('/install/.quota.lock')) {
